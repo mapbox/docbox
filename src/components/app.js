b1ea939d9fe340926063cbefdbac81507e4bc0c2
Daniel Destouche <daniel.destouche@gmail.com>
2017-09-22 14:59:04 -0400

    Remove debugger

--- src/components/app.js
+++ src/components/app.js
@@ -146,7 +146,6 @@ export default class App extends React.PureComponent {
     }
   }
   navigationItemClicked = activeSection => {
-      debugger;
     setTimeout(() => {
       this.setState({ activeSection });
     }, 10);
